21c3a3f81c49d130d72804ebe5ba772de50f5a0a
NEEL-SAMEL <neelsamel69@gmail.com>
2024-03-19 08:14:56 +0530

    front-end and back-end uploaded

---
+++ back-end/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+app.use(morgan("dev"))
+
+
+const port = process.env.PORT || 8080
+app.listen(port,()=>{
+    console.log(`port is running on ${port}`)
+})
