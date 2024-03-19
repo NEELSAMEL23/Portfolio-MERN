f85dd2ded694bcf32305842bd7ca98d2805cf786
NEELSAMEL23 <162904525+NEELSAMEL23@users.noreply.github.com>
2024-03-19 20:31:22 +0530

    Front and back end updated to deploy app

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,19 @@
-const express = require("express")
-const morgan = require("morgan")
-const dotenv = require("dotenv")
-dotenv.config()
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+const path = require("path");
 
-const app = express()
-app.use(express.json())
-app.use(morgan("dev"))
-app.use("/api",require("./routes/contactRoutes"))
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/api", require("./routes/contactRoutes"));
+app.use(express.static(path.join(__dirname, "../front-end/build")));
 
-const port = process.env.PORT || 8080
-app.listen(port,()=>{
-    console.log(`port is running on ${port}`)
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname,"../front-end/build/index.html"));
+});
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`port is running on ${port}`);
+});
